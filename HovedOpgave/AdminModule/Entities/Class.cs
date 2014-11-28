46c6474f0497da3efc00e41e9b839d195ddce441
layior <lars.s.jensen92@gmail.com>
2014-11-28 10:47:46 +0100

    for accepteance test

--- HovedOpgave/AdminModule/Entities/Class.cs
+++ HovedOpgave/AdminModule/Entities/Class.cs
@@ -8,11 +8,40 @@ namespace AdminModule.Webservice
 {
     public partial class Class
     {
+        //shouldwork...
+    /*    private string associatedTeacher;
+
+        public string AssociatedTeacher
+        {
+            get
+            {
+                if (ObjectHolder.Instance.TeacherList != null)
+                {
+
+                    foreach (TeacherEx teacher in ObjectHolder.Instance.TeacherList)
+                    {
+                        if (teacher.Id==Id)
+                        {
+                            associatedTeacher = teacher.Firstname + " " + teacher.Lastname;
+                        }
+                    }
+                }
+                else
+                {
+                    ObjectHolder.Instance.GetTeachers();
+                }
+
+                return associatedTeacher;
+            }
+            set { associatedTeacher = value; }
+        }
+
+        */
 
         public override string ToString()
         {
-            
-            return Name+"\tUnderviser:"+Fkteacherid;
+
+            return Name + "\tUnderviser:" + Fkteacherid;
         }
 
 
