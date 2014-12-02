a3d0a951e3c4eec81d37347860b9ff8388b96bd9
Patrick Larsen <hatsen2000@gmail.com>
2014-12-02 12:44:03 +0100

    MVC5 website.

---
+++ HovedOpgave/BirkealleWebsite/Controllers/NavigationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirkealleWebsite.Controllers
+{
+    public class NavigationController : Controller
+    {
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            return View("~/Views/Navigation/IndexView.cshtml");
+        }
+
+        public ActionResult GotoEnrollment()
+        {
+            return View("~/Views/Navigation/EnrollmentView.cshtml");
+        }
+
+        public ActionResult GotoAboutSchool()
+        {
+            return View("~/Views/Navigation/AboutSchoolView.cshtml");
+        }
+
+        public ActionResult GotoLogin()
+        {
+            return View("~/Views/Navigation/LoginView.cshtml");
+        }
+
+        public ActionResult GotoContact()
+        {
+            return View("~/Views/Navigation/ContactView.cshtml");
+        }
+
+        public ActionResult GotoGallery()
+        {
+            return View("~/Views/Navigation/GalleryView.cshtml");
+        }
+    }
+}
